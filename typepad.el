8e796d3abf91d13a51d2e15f585329faff936ee3
weland <weland@bupt.edu.cn>
2024-01-08 17:52:38 +0800

    diff setup

--- typepad.el
+++ typepad.el
@@ -60,4 +60,57 @@
           (lambda ()
             (add-hook 'post-command-hook 'typepad-count-key nil t)))
 
+;; define the face for diff highlight
+(defface typepad-delete-face
+  '((t (:background "red")))
+  "Face for delete text in diff.")
+(defface typepad-insert-face
+  '((t (:background "green")))
+  "Face for insert text in diff.")
+
+
+
+;; define diff-string function
+(defun diff-string (str1 str2)
+  "Return a list of diff between str1 and str2."
+  (let ((diff (compare-list
+                (split-string str1 "")
+                (split-string str2 "")
+                )))
+    (dolist (item diff)
+      (let ((type (car (cdr item))))
+        (when (not (eq type ?=))
+          (setcar (cdr item) (car item))
+          (setcar item (car (cdr item))))))
+    diff))
+
+;; use overlay to highlight diff of the two buffer
+(defun typepad-highlight-diff ()
+  (interactive)
+  (let ((writable-buffer (get-buffer "*跟打区*"))
+        (readonly-buffer (get-buffer "*发文区*")))
+    (when (and writable-buffer readonly-buffer)
+      (let ((writable-buffer-text (buffer-substring-no-properties
+                                   (point-min) (point-max)))
+            (readonly-buffer-text (with-current-buffer readonly-buffer
+                                    (buffer-substring-no-properties
+                                     (point-min) (point-max)))))
+        (when (not (string= writable-buffer-text readonly-buffer-text))
+          (let ((diff (diff-string writable-buffer-text readonly-buffer-text)))
+            (with-current-buffer writable-buffer
+              (remove-overlays (point-min) (point-max) 'typepad t)
+              (dolist (item diff)
+                (let ((start (car item))
+                      (end (car (cdr item)))
+                      (type (car (cdr (cdr item)))))
+                  (overlay-put (make-overlay start end) 'typepad t)
+                  (overlay-put (make-overlay start end) 'face
+                               (cond ((eq type ?-) 'typepad-delete-face)
+                                 ((eq type ?+) 'typepad-insert-face))))))))))))
+
+;; do the highlight when the writable buffer changed
+(add-hook 'typepad-mode-hook
+          (lambda ()
+            (add-hook 'post-command-hook 'typepad-highlight-diff nil t)))
+
 
