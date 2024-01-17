0f94ed4366f4fa6dc977e010048addf947198e1b
weland <weland@bupt.edu.cn>
2024-01-17 18:09:32 +0800

    update eask

--- typepad.el
+++ typepad.el
@@ -1,3 +1,35 @@
+;;; typepad.el --- typepad in Emacs  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  welandx
+
+;; Author: welandx <{ welandx@skiff.com }>
+;; URL: https://github.com/welandx/TypePad.el
+;; Version: 0.1
+;; Package-Requires: ((emacs "29.1") (pyim))
+;; Keywords: typing
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; typepad in Emacs
+;;
+
+;;; Code:
+
 (require 'typepad-lib)
 
 (defgroup typepad ()
@@ -393,7 +425,10 @@
 
 (defun typepad-sql-indices-save (indices)
   (let* ((serialized (prin1-to-string indices))
-          (encoded (base64-encode-string serialized)))))
+          (encoded (base64-encode-string serialized))
+          (tp-db (sqlite-open (concat user-emacs-directory "typepad.db"))))
+    (sqlite-execute tp-db
+      "INSERT INTO sort (hash, p, encoded)")))
 
 (defun typepad-sql-indices-read ()
   )
@@ -403,4 +438,4 @@
 (require 'typepad-time)
 
 (provide 'typepad)
-;;; typepad.el end here
+;;; typepad.el ends here
